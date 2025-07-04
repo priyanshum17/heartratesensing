53d7deb990f752332e3b3a4fa7441d4b2030d080
Priyanshu Mehta <70831431+priyanshum17@users.noreply.github.com>
2025-07-03 23:54:55 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,124 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+import 'package:path/path.dart' as Path;
+import 'package:sqflite/sqflite.dart';
+import 'package:flutter/widgets.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:path_provider/path_provider.dart';
+
+// This tutorial could help you understand more about building an app with Flutter.
+// https://codelabs.developers.google.com/codelabs/flutter-codelab-first#0
+// It is recommended but not required.
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'IMU Data Recording',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  // TODO: Add a boolean variable, recording, to the help indicate whether we are recording the sensor data.
+  
+
+  // TODO: Add all variables for retrieving sensor data events, update times, and intervals
+  // TODO: Initiate _streamSubscriptions to retrieve all StreamSubscriptions in an array.
+
+
+  // TODO: Initiate a DatabaseHelper instance. 
+  // DatabaseHelper class is at the end of this file and requires your implementation.
+
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // return a Scaffold object for the UI elements.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: 
+      );
+  }
+
+  // TODO: implement this function to stop recording sensor data by cancelling the _streamSubscriptions
+  void stopRecording() {
+    
+  }
+
+  // TODO: start recording sensor data by adding subscriptions for
+  // UserAccelerometerEvent, accelerometerEventStream, gyroscopeEventStream, magnetometerEventStream
+  // use StreamSubscriptions.add function
+  void startRecording() {
+
+  }
+}
+
+// Define a class with all desired variables to store in the table
+class ImuData {
+  // TODO: Define all variables in this class here
+
+
+  // Define the initiation of the class instance
+  ImuData({
+
+  });
+
+  // TODO: Convert a IMU data into a Map. The keys must correspond to the names of the
+  // columns in the database.
+  Map<String, Object?> toMap() {
+    return {
+      
+    };
+  }
+}
+
+// Define a DatabaseHelper class 
+class DatabaseHelper {
+  // TODO: create static variables to store to the same database 
+  // and construct a global/static DatabaseHelper instance
+
+
+  // TODO: Define the function to get all items from database
+  Future<Database> get database async {
+
+  }
+
+  // TODO: Open the database and create the table if it doesn't exist.
+  _initDatabase() async {
+
+  }
+
+  // TODO: SQL code to create the database table.
+  Future _onCreate(Database db, int version) async {
+    
+  }
+
+  // TODO: Method to insert IMU data into the database.
+  Future<void> insertImuData(ImuData imuData) async {
+    
+  }
+
+  // TODO: Write a function to export the database using Share.shareXFiles()
+  Future<void> exportDatabase() async {
+
   }
 }
